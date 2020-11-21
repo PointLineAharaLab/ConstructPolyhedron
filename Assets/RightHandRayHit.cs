1f65e09a4bbd975a2f290c6efc92fb7924e4ffba
Kazushi Ahara <ahara@meiji.ac.jp>
2020-11-21 12:46:57 +0900

    1121

--- Assets/RightHandRayHit.cs
+++ Assets/RightHandRayHit.cs
@@ -7,11 +7,17 @@ public class RightHandRayHit : MonoBehaviour
     Ray ray;
     public GameObject RightHandAnchor;
     Collider col;
+    public GameObject RHGObject;
+    public Vector3 RHGPosition;
+    public Vector3 RHGHitPosition;
+    public float RHGDistance;
+    public bool RHGon;
     // Start is called before the first frame update
     void Start()
     {
         ray = new Ray();
-
+        RHGon = false;
+        RHGPosition = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -23,13 +29,49 @@ public class RightHandRayHit : MonoBehaviour
         bool hitYN = Physics.Raycast(ray, out RaycastHit hitInfo);
 
         if (hitYN)
-		{
+        {
             transform.position = hitInfo.point;
-            col = hitInfo.collider;
-		}
-        else
+            if (!hitInfo.collider.name.Contains("Ground"))
+            {
+                Debug.Log(hitInfo.collider.name);
+                if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+                {
+                    //Debug.Log("A");
+                    RHGon = true;
+                    RHGObject = hitInfo.collider.gameObject;
+                    RHGPosition = RHGObject.transform.position;
+                    RHGHitPosition = hitInfo.point;
+                    RHGDistance = (ray.origin - RHGHitPosition).magnitude;
+                }
+            }
+        }
+		if (RHGon)
 		{
-            transform.position = Vector3.down;
+            if (OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+			{
+                RHGObject.transform.position = ray.origin + RHGDistance * ray.direction + (RHGPosition - RHGHitPosition);//ヒットしている位置
+            }
+            else
+			{
+                RHGon = false;
+                RHGPosition = Vector3.zero;
+                transform.position = Vector3.down;
+            }
+        }
+        if(!hitYN && !RHGon)// ヒットしていないし、グラブもしていない。
+		{
+			if (OVRInput.GetDown(OVRInput.Button.One))
+			{
+                AddVertex(ray.origin + ray.direction);
+			}
 		}
     }
+
+    public void AddVertex(Vector3 position)
+	{
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/Vertex");
+        GameObject vertexObject = Instantiate(prefab,position, Quaternion.identity);
+
+
+	}
 }
---
+++ Assets/Vertex.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vertex : MonoBehaviour
+{
+    public Color color;
+
+    float scale = 0.25f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
